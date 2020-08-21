315e4ed85cbbb7f23d0c582e258c701a8ef86ba1
BirjuVachhani <brvachhani@gmail.com>
2020-08-21 11:26:26 +0530

    :green_heart: add publish workflow

---
+++ pub_login.sh
@@ -0,0 +1,35 @@
+# This script creates/updates credentials.json file which is used
+# to authorize publisher when publishing packages to pub.dev
+
+# Checking whether the secrets are available as environment
+# variables or not.
+if [ -z "${PUB_DEV_PUBLISH_ACCESS_TOKEN}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_ACCESS_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_REFRESH_TOKEN}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_REFRESH_TOKEN environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_TOKEN_ENDPOINT environment variable"
+  exit 1
+fi
+
+if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
+  echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
+  exit 1
+fi
+
+# Create credentials.json file.
+cat <<EOF >~/.pub-cache/credentials.json
+{
+  "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
+  "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
+  "tokenEndpoint":"${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}",
+  "scopes":["https://www.googleapis.com/auth/userinfo.email","openid"],
+  "expiration":${PUB_DEV_PUBLISH_EXPIRATION}
+}
+EOF

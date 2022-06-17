e2b868e991db3212ed0d03376b58577e8078a0e0
Parth Baraiya <36261739+ParthBaraiya@users.noreply.github.com>
2022-06-17 18:01:32 +0530

    :fix: update CHANGELOG.md (#225)
    - Provide support for older versions of flutter as well.
    
    Co-authored-by: Parth Baraiya <parth.b@simformsolutions.com>

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -20,48 +20,5 @@
  * SOFTWARE.
  */
 
-import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-
-import 'extension.dart';
-
-typedef OnWidgetSizeChange = void Function(Size? size);
-
-class MeasureSize extends StatefulWidget {
-  final Widget? child;
-  final OnWidgetSizeChange onSizeChange;
-
-  MeasureSize({
-    required this.onSizeChange,
-    required this.child,
-  });
-
-  @override
-  _MeasureSizeState createState() => _MeasureSizeState();
-}
-
-class _MeasureSizeState extends State<MeasureSize> {
-  @override
-  Widget build(BuildContext context) {
-    ambiguate(SchedulerBinding.instance)
-        ?.addPostFrameCallback(postFrameCallback);
-    return Container(
-      key: widgetKey,
-      child: widget.child,
-    );
-  }
-
-  GlobalKey widgetKey = GlobalKey();
-  Size? oldSize;
-
-  void postFrameCallback(Duration timestamp) {
-    var context = widgetKey.currentContext;
-    if (context == null) return;
-
-    var newSize = context.size;
-    if (oldSize == newSize) return;
-
-    oldSize = newSize;
-    widget.onSizeChange(newSize);
-  }
-}
+// TODO: remove this function if we remove support for flutter 2.x
+T? ambiguate<T>(T? object) => object;

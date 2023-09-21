25c88ffbc95b9ca398d23548d3acfc8713beaf7d
Michael Cadavillo <michael.cadavillo@gmail.com>
2023-09-21 16:25:23 +0800

    feat: ✨Add staticContainer into ToolTipWidget for a custom tooltip that can be placed anywhere on screen and is not affected by the animation
    Signed-off-by: Michael Angelo Cadavillo <michael.cadavillo@gmail.com>

--- lib/src/showcase.dart
+++ lib/src/showcase.dart
@@ -96,6 +96,9 @@ class Showcase extends StatefulWidget {
   /// Custom tooltip widget when [Showcase.withWidget] is used.
   final Widget? container;
 
+  /// Custom static tooltip widget when [Showcase.withWidget] is used.
+  final Widget? staticContainer;
+
   /// Defines background color for tooltip widget.
   ///
   /// Default to [Colors.white]
@@ -418,6 +421,7 @@ class Showcase extends StatefulWidget {
   })  : height = null,
         width = null,
         container = null,
+        staticContainer = null,
         assert(overlayOpacity >= 0.0 && overlayOpacity <= 1.0,
             "overlay opacity must be between 0 and 1."),
         assert(onTargetClick == null || disposeOnTap != null,
@@ -484,6 +488,7 @@ class Showcase extends StatefulWidget {
     required this.width,
     required this.container,
     required this.child,
+    this.staticContainer,
     this.targetShapeBorder = const RoundedRectangleBorder(
       borderRadius: BorderRadius.all(
         Radius.circular(8),
@@ -789,6 +794,7 @@ class _ShowcaseState extends State<Showcase> {
             titleTextStyle: widget.titleTextStyle,
             descTextStyle: widget.descTextStyle,
             container: widget.container,
+            staticContainer: widget.staticContainer,
             tooltipBackgroundColor: widget.tooltipBackgroundColor,
             textColor: widget.textColor,
             showArrow: widget.showArrow,

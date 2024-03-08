bb599415147974de7a5201ce2176e5f6ae70ecb6
Sahil-Simform <sahil.t@simformsolutions.com>
2024-03-08 15:33:21 +0530

    fix: 🐛 Updated `IgnorePointer` from `AbsorbPointer` to send HitTest to targeted widget
    - Updated `IgnorePointer` from `AbsorbPointer` to send HitTest to targeted widget as `AbsorbPointer` cancel all type of user interaction and `IgnorePointer`only cancel it's closest child's interaction( in our case `GestureDetector` used on child) and passes it to below widget(in our case target widget)

--- lib/src/showcase.dart
+++ lib/src/showcase.dart
@@ -656,7 +656,7 @@ class _TargetWidget extends StatelessWidget {
       top: offset.dy,
       left: offset.dx,
       child: disableDefaultChildGestures
-          ? AbsorbPointer(
+          ? IgnorePointer(
               child: targetWidgetContent(),
             )
           : targetWidgetContent(),
